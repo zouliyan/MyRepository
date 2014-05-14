d19464e85c0f1fdfb6e99a59a461fd7fb220d8f7
zouliyan <z_liyan@sina.com>
2014-05-14 10:31:39 +0800

    原始工程。
    2014/5/14/邹立演

---
+++ StuGit/StuGit/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  StuGit
+//
+//  Created by ibokan on 14-5-14.
+//  Copyright (c) 2014年 Z_Liyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
